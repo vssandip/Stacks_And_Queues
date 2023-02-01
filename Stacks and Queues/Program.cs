515842e0ce98b2140c5c0f82b5ab6bdc5ca4dce4
“Vssandip” <sandipshindesr95035@gmail.com>
2023-02-01 12:49:18 +0530

    [ SANDIP ] Adding  'UC2_Stacks'

--- Stacks and Queues/Program.cs
+++ Stacks and Queues/Program.cs
@@ -5,10 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("#$#$ Welcome To Stacks and Queues #$#$#");
-            StackQueue a = new StackQueue();
+           /* StackQueue a = new StackQueue();
             a.push(70);
             a.push(30);
             a.push(56);
+            a.show();*/
+            Stack a = new Stack();
+            a.push(70);
+            a.push(30);
+            a.push(56);
+            a.pop();
+            a.pop();
+            a.pop();
+            a.pop();
             a.show();
         }
     }
---
+++ Stacks and Queues/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_and_Queues
+{
+    internal class Stack
+    {
+        public Node top;
+        public void push(int data)
+        {
+            Node node = new Node();
+            node.data = data;
+            if (top == null)
+            {
+                top = node;
+            }
+            else
+            {
+                node.next = top;
+                top = node;
+            }
+
+
+        }
+        public void pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("The stack is empty");
+            }
+            else
+            {
+                top = top.next;
+            }
+        }
+        public void show()
+        {
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
