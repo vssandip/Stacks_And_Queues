baa652cd268fa494d64b38a56599482445e94720
“Vssandip” <sandipshindesr95035@gmail.com>
2023-02-01 13:07:24 +0530

    [ SANDIP ] Adding  'UC3_Queues'

--- Stacks and Queues/Program.cs
+++ Stacks and Queues/Program.cs
@@ -5,19 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("#$#$ Welcome To Stacks and Queues #$#$#");
-           /* StackQueue a = new StackQueue();
+            /* StackQueue a = new StackQueue();
+             a.push(70);
+             a.push(30);
+             a.push(56);
+             a.show();*/
+            /*Stack a = new Stack();
             a.push(70);
             a.push(30);
             a.push(56);
+            a.pop();
+            a.pop();
+            a.pop();
+            a.pop();
             a.show();*/
-            Stack a = new Stack();
-            a.push(70);
-            a.push(30);
-            a.push(56);
-            a.pop();
-            a.pop();
-            a.pop();
-            a.pop();
+            UC1_Queues a = new UC1_Queues ();
+            a.enqueue(56);
+            a.enqueue(30);
+            a.enqueue(70);
             a.show();
         }
     }
---
+++ Stacks and Queues/UC1_Queues.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_and_Queues
+{
+    internal class UC1_Queues
+    {
+        public Node front;
+        public Node rare;
+        public void enqueue(int data)
+        {
+            Node node = new Node();
+            node.data = data;
+            if (front == null)
+            {
+                front = node;
+                rare = node;
+            }
+            else
+            {
+                rare.next = node;
+                rare = node;
+            }
+
+
+        }
+        public void show()
+        {
+            Node temp = front;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
